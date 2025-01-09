b1fe331069a90bacad671166df1a766f71674cb8
DanialHuckabee <efeo@seneka.com.tr>
2025-01-09 16:52:18 +0300

    sss

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -127,52 +127,7 @@ const groupedUsers = computed(() => {
       </div>
     </div>
 
-    <div id="userList" class="mt-8">
-      <div class="px-4 sm:px-6 lg:px-8">
-        <div class="sm:flex sm:items-center">
-          <div class="sm:flex-auto">
-            <h1 class="text-base font-semibold text-gray-900">Tüm Kullanıcılar</h1>
-            <p class="mt-2 text-sm text-gray-700">Eşleşen tüm kullanıcıların listesi.</p>
-          </div>
-        </div>
-        <div class="mt-8 flow-root">
-          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <table class="min-w-full divide-y divide-gray-300">
-                <thead>
-                  <tr>
-                    <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Source User</th>
-                    <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Target User</th>
-                    <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Organization</th>
-                    <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                  <tr v-for="user in users" :key="user.sourceUser.id" class="hover:bg-gray-50">
-                    <td class="whitespace-nowrap py-2 pl-4 pr-3 sm:pl-0">
-                      <div class="text-sm text-gray-900">{{ user.sourceUser.displayName || '-' }}</div>
-                      <div class="text-xs text-gray-500">ID: {{ user.sourceUser.id }}</div>
-                    </td>
-                    <td class="whitespace-nowrap px-2 py-2">
-                      <div class="text-sm text-gray-900">{{ user.targetUser.displayName || '-' }}</div>
-                      <div class="text-xs text-gray-500">ID: {{ user.targetUser.id }}</div>
-                    </td>
-                    <td class="whitespace-nowrap px-2 py-2">
-                      <div class="text-sm text-gray-900">{{ user.sourceUser.organizationDisplayName || '-' }}</div>
-                      <div class="text-xs text-gray-500">
-                        <span class="block">Source Org ID: {{ user.sourceUser.organizationId || '-' }}</span>
-                        <span class="block">Target Org ID: {{ user.targetUser.organizationId || '-' }}</span>
-                      </div>
-                    </td>
-                    <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{ user.sourceUser.email }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+   
 
     <OrganizationCompareModal
       :is-open="showOrgCompareModal"
